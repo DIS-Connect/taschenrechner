fa5e3ca474abfe37a565727cc0bde89faeafb763
Paul <paul.reisenberg@web.de>
2017-11-05 12:41:31 +0100

    simple und anvanced

--- src/Rechner1.java
+++ src/Rechner1.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 
-public class Rechner2 extends JFrame {
+public class Rechner1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
@@ -30,9 +30,9 @@ public class Rechner2 extends JFrame {
 	private boolean wurzel;
 	private JRadioButton keepresult = new JRadioButton();
 	private JTextField ergebnisFeld = new JTextField();
-	private JPanel advanced = new JPanel();
+	private JPanel simple = new JPanel();
 
-	public Rechner2() {
+	public Rechner1() {
 
 		super();
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -46,17 +46,17 @@ public class Rechner2 extends JFrame {
 		setTitle("Rechner2");
 		setResizable(false);
 	
-		advanced.setLayout(null);
+		simple.setLayout(null);
 		// Anfang Komponenten
 
 		fOperant1.setBounds(20, 40, 80, 40);
-		advanced.add(fOperant1);
+		simple.add(fOperant1);
 		operator.setBounds(120, 40, 40, 40);
 		operator.setFont(new Font("Dialog", Font.PLAIN, 20));
 		operator.setHorizontalAlignment(SwingConstants.CENTER);
-		advanced.add(operator);
+		simple.add(operator);
 		fOperant2.setBounds(180, 40, 80, 41);
-		advanced.add(fOperant2);
+		simple.add(fOperant2);
 		ergebnisKnopf.setBounds(280, 40, 50, 40);
 		ergebnisKnopf.setText("=");
 		ergebnisKnopf.setMargin(new Insets(2, 2, 2, 2));
@@ -66,7 +66,7 @@ public class Rechner2 extends JFrame {
 			}
 		});
 		ergebnisKnopf.setFont(new Font("Dialog", Font.BOLD, 20));
-		advanced.add(ergebnisKnopf);
+		simple.add(ergebnisKnopf);
 
 		plusKnopf.setBounds(25, 125, 50, 50);
 		plusKnopf.setText("+");
@@ -77,7 +77,7 @@ public class Rechner2 extends JFrame {
 			}
 		});
 		plusKnopf.setFont(new Font("Dialog", Font.BOLD, 20));
-		advanced.add(plusKnopf);
+		simple.add(plusKnopf);
 
 		minusKnopf.setBounds(25, 200, 50, 50);
 		minusKnopf.setText("-");
@@ -88,7 +88,7 @@ public class Rechner2 extends JFrame {
 			}
 		});
 		minusKnopf.setFont(new Font("Dialog", Font.BOLD, 20));
-		advanced.add(minusKnopf);
+		simple.add(minusKnopf);
 		malKnopf.setBounds(100, 125, 50, 50);
 		malKnopf.setText("X");
 		malKnopf.setMargin(new Insets(2, 2, 2, 2));
@@ -99,7 +99,7 @@ public class Rechner2 extends JFrame {
 		});
 		malKnopf.setHorizontalTextPosition(SwingConstants.CENTER);
 		malKnopf.setFont(new Font("Dialog", Font.BOLD, 18));
-		advanced.add(malKnopf);
+		simple.add(malKnopf);
 		teilKnopf.setBounds(100, 200, 50, 50);
 		teilKnopf.setText("/");
 		teilKnopf.setMargin(new Insets(2, 2, 2, 2));
@@ -109,7 +109,7 @@ public class Rechner2 extends JFrame {
 			}
 		});
 		teilKnopf.setFont(new Font("Dialog", Font.BOLD, 18));
-		advanced.add(teilKnopf);
+		simple.add(teilKnopf);
 		hoch.setBounds(175, 125, 50, 50);
 		hoch.setText("^");
 		hoch.setMargin(new Insets(2, 2, 2, 2));
@@ -120,7 +120,7 @@ public class Rechner2 extends JFrame {
 		});
 		hoch.setHorizontalTextPosition(SwingConstants.CENTER);
 		hoch.setFont(new Font("Dialog", Font.BOLD, 20));
-		advanced.add(hoch);
+		simple.add(hoch);
 
 		wurzelKnopf.setBounds(175, 200, 50, 50);
 		wurzelKnopf.setText("√");
@@ -131,51 +131,10 @@ public class Rechner2 extends JFrame {
 
 			}
 		});
-		keepresult2.setBounds(350, 125, 180, 25);
-		keepresult2.setOpaque(false);
-		keepresult2.setText("keep result in field 2");
-        keepresult2.setActionCommand("2");
-	    advanced.add(keepresult2);
-        keepRadioGroup.add(keepresult2);
-        
-        keepresult1.setBounds(350, 100, 180, 25);
-	    keepresult1.setOpaque(false);
-	    keepresult1.setText("keep result in field 1");
-        keepresult1.setActionCommand("1");
-        keepRadioGroup.add(keepresult1);
-	    advanced.add(keepresult1);
-		wurzelKnopf.setFont(new Font("Dialog", Font.BOLD, 18));
-		advanced.add(wurzelKnopf);
 		
-		keepInput.setBounds(350, 50, 180, 25);
-	    keepInput.setOpaque(false);
-        keepInput.setActionCommand("keep Input");
-	    keepInput.setText("keep input");
-	    advanced.add(keepInput);
-        keepRadioGroup.add(keepInput);
-
-        keepNothing.setBounds(350, 75, 180, 25);
-        keepNothing.setOpaque(false);
-        keepNothing.setActionCommand("3");
-        keepNothing.setText("keep nothing");
-        keepNothing.setSelected(true);
-        advanced.add(keepNothing);
-        keepRadioGroup.add(keepNothing);
-        
-        keepresult.setBounds(350, 150, 180, 25);
-        keepresult.setOpaque(false);
-        keepresult.setActionCommand("4");
-        keepresult.setText("keep result");
-        keepresult.setSelected(true);
-        advanced.add(keepresult);
-        keepRadioGroup.add(keepresult);
-        
-         ergebnisFeld.setBounds(350, 175, 90, 40);
-		advanced.add(ergebnisFeld);
 		// Ende Komponenten
-
+      add (simple);
 		setVisible(true);
-		add(advanced);
 	} // end of public Rechner2
 
 	// Anfang Methoden
@@ -186,7 +145,7 @@ public class Rechner2 extends JFrame {
 
 	public void responseFunktion(String ergebnisB) {
 
-		JOptionPane.showMessageDialog(Rechner2.this, "Ergebnis: " + ergebnisB, "Berechnung",
+		JOptionPane.showMessageDialog(Rechner1.this, "Ergebnis: " + ergebnisB, "Berechnung",
 				JOptionPane.INFORMATION_MESSAGE);
 
 	}
@@ -305,28 +264,9 @@ public class Rechner2 extends JFrame {
 			}
 
 		}
-        if (keepRadioGroup.getSelection().getActionCommand() == "1") {
-            fOperant1.setText(Double.toString(ergebnisZ));
-            System.out.println(Double.toString(ergebnisZ));
-            fOperant2.setText("");
-            operator.setText("");
-        } else if (keepRadioGroup.getSelection().getActionCommand() == "2") {
-            fOperant2.setText(Double.toString(ergebnisZ));
-            fOperant1.setText("");
-            operator.setText("");
-        } else if ((keepRadioGroup.getSelection().getActionCommand() == "3")) {
-            fOperant1.setText("");
-            fOperant2.setText("");
-            operator.setText("");
-        }
-        else if ((keepRadioGroup.getSelection().getActionCommand() == "4")) {
-        	
-        	ergebnisFeld.setText(Double.toString(ergebnisZ));
-        	fOperant1.setText("");
-            fOperant2.setText("");
-            operator.setText("");
-        }
+       
 	}
 		
 		// Ende Methoden
-} // end of class Rechner2
+} // end of class Rechner1
+
